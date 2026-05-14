748181fae8c19fee9d5ded43ab9ce597d2d55650
Your Name <pborkar0003@gmail.com>
2026-05-14 12:03:40 +0530

    removed test code

--- app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("pages/home.tsx"),
-  route("login", "pages/login.tsx"),
-] satisfies RouteConfig;
